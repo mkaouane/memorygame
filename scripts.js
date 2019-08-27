cbfd7bc128db31e5c72200e6d23492e5cdfcdb78
Kaouane Mohamed <mkaouane1@gmail.com>
2019-08-27 16:22:22 +0200

    ajout de son

--- scripts.js
+++ scripts.js
@@ -1,4 +1,36 @@
+class AudioController {
+  constructor() {
+      this.bgMusic = new Audio('audio/bgmusic.mp3');
+      this.flipSound = new Audio('audio/flip.wav');
+      this.victorySound = new Audio ('audio/victory.mp3')
+      this.matchedSound = new Audio('audio/matched.mp3');
+      this.victorySound.volume = 0.1;
+      this.matchedSound.volume= 0.1;
+      this.bgMusic.volume = 0.2;
+      this.bgMusic.loop = true;
 
+  }
+
+  soundVictory() {
+    this.victorySound.play();
+  }
+  
+  startMusic() {
+     // this.bgMusic.play();
+  }
+
+  soundflip() {
+    this.flipSound.play();
+  }
+
+  soundMatch() {
+    this.matchedSound.play();
+  }
+}
+
+let audioController = new AudioController;
+
+audioController.startMusic();
 
 const cards = document.querySelectorAll('.memory-card');
 
@@ -6,11 +38,14 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+
+
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
 
   this.classList.add('flip');
+  audioController.soundflip();
 
   if (!hasFlippedCard) {
     // first click
@@ -28,13 +63,14 @@ function flipCard() {
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
   isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  audioController.soundMatch();
+
 
   resetBoard();
 }
@@ -53,6 +89,7 @@ function unflipCards() {
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
+
 }
 
 (function shuffle() {
